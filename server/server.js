da0c75312fa7a88848646dcc22c1d90cb8d8591e
mininpark <parkmini.93@gmail.com>
2022-12-27 18:16:04 +0100

    built backend server with express

---
+++ server/server.js
@@ -0,0 +1,50 @@
+// to be able to code OpenAPI
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuaration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openAi = new OpenAIApi(configuaration)
+
+const app = express()
+app.use(cors())
+//pass the json frontend to backend
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from MinaX',
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt
+    const response = await openAi.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0.3,
+      max_tokens: 1,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    })
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ error })
+  }
+})
+
+app.listen(3000, () =>
+  console.log('Server is running on port http://localhost:3000')
+)
